3bc9fec13dee94cb13839cf453e89aab206b26fc
Vikram Rawlo <vikramrawlo@gmail.com>
2019-04-12 13:28:49 +0530

    Added flake8 and travisCI config

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,17 +1,29 @@
 from config.settings.base import *
 
-
-try:
-    from config.settings.local import *
-except:
-    SECRET_KEY = 'bo+xba@bth5!wfue6p&f2-$l9kuv9nv1(pdq8+!u3ars61l!qr'
-    
+# Travis Setup
+if 'TRAVIS' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'travisdb',
+            'USER':     'postgres',
+            'PASSWORD': '',
+            'HOST':     'localhost',
+            'PORT':     '',
         }
     }
+else:
+    try:
+        from config.settings.local import *
+    except:
+        SECRET_KEY = 'bo+xba@bth5!wfue6p&f2-$l9kuv9nv1(pdq8+!u3ars61l!qr'
+        
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            }
+        }
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
