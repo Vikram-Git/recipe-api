167d86280c66edf3bec7aa4e7c85675609e4e20d
Vikram Rawlo <vikramrawlo@gmail.com>
2019-04-12 14:43:17 +0530

    solved flake8 detected errors

--- config/urls.py
+++ config/urls.py
@@ -9,7 +9,11 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-        document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.STATIC_URL,
-        document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+        )
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.MEDIA_ROOT
+        )
