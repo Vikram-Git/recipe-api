682b5b1c4e63ea23946d7e09e182c5801ac37743
Vikram Rawlo <vikramrawlo@gmail.com>
2019-04-17 13:01:28 +0530

    Updated admin to support custom user model

--- core/admin.py
+++ core/admin.py
@@ -7,8 +7,8 @@ from core import models
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
-    
-    '''Django comes with default 'User' edit page, we have to modify it to our custom page'''
+
+    '''Django comes with default 'User' edit page, we will customize it'''
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         (('Personal Info'), {'fields': ('name',)}),
@@ -19,12 +19,13 @@ class UserAdmin(BaseUserAdmin):
         (('Important Dates'), {'fields': ('last_login',)})
     )
 
-    '''Django comes with default fields to create a user, we will customize it'''
+    '''admin comes with default fields to create user, we will customize it'''
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
             'fields': ('email', 'password1', 'password2')
         }),
     )
+
 
 admin.site.register(models.User, UserAdmin)
